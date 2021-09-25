b19dff3a59bd9aadc8700609f7f2850a2c9abb9f
utente <utente@LAPTOP-7KT8GAIP>
2021-09-25 12:44:26 +0200

    Aggiunte le eccezioni

--- IndovinaNumeroMVP/src/controller/Controller.java
+++ IndovinaNumeroMVP/src/controller/Controller.java
@@ -11,41 +11,47 @@ import view.View;
 public class Controller implements ActionListener {
 	private Numeri n;
 	private View v;
+	private int num;
 
 	public Controller(Numeri n, View v) {
 		v.registraActionListener(this);
 		this.n = n;
 		this.v = v;
 	}
-	
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		v.getLblTentativi().setText( "Tentativi: " + n.getTentativi());
+		v.getLblTentativi().setText("Tentativi: " + n.getTentativi());
 		// n.diminuisciTentativi();
 		if (arg0.getActionCommand().equalsIgnoreCase("Invia")) {
 			String n1 = v.insNumero.getText();
-			int num = Integer.parseInt(n1);
-			if (num < n.getNumero()) {
-				v.getLblTentativi().setText( "Tentativi: " + n.diminuisciTentativi());
-				JOptionPane.showMessageDialog(null, "Il numero è più grande");
-				v.insNumero.setText("");
 
-			} else if (num > n.getNumero()) {
-				v.getLblTentativi().setText("Tentativi: "+ n.diminuisciTentativi());
-				JOptionPane.showMessageDialog(null, "Il numero è più piccolo");
-				v.insNumero.setText("");
-			} else {
-				JOptionPane.showMessageDialog(null, "Hai vinto!!");
-				System.exit(0);
+			try {
+				num = Integer.parseInt(n1);
+				if (num < n.getNumero() && v.insNumero.getText() != "") {
+					v.getLblTentativi().setText("Tentativi: " + n.diminuisciTentativi());
+					JOptionPane.showMessageDialog(null, "Il numero è più grande");
+					v.insNumero.setText("");
+
+				} else if (num > n.getNumero() && v.insNumero.getText() != "") {
+					v.getLblTentativi().setText("Tentativi: " + n.diminuisciTentativi());
+					JOptionPane.showMessageDialog(null, "Il numero è più piccolo");
+					v.insNumero.setText("");
+				} else {
+					JOptionPane.showMessageDialog(null, "Hai vinto!!");
+					System.exit(0);
+				}
+				if (n.getTentativi() != 0) {
+					System.out.println("");
+				} else {
+					JOptionPane.showMessageDialog(null, "Hai finito i tentativi");
+					System.exit(0);
+				}
+			} catch (Exception e) {
+				System.out.println("Devi inserire un numero!");
+
 			}
-			if (n.getTentativi() != 0){
-				System.out.println("");
-			} else {
-				JOptionPane.showMessageDialog(null,"Hai finito i tentativi");
-				System.exit(0);
-			}
-			
+
 		}
 
 	}
